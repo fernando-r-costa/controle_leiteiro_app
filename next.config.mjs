725cf8ce581335ddf0c27b45d0b9b3e80e5ed58a
fernando-r-costa <fernando.r.costa@outlook.com>
2026-09-19 17:37:53 -0300

    fix: correct PWA install configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   register: true,
-  scope: "/app",
+  scope: "/",
   sw: "service-worker.js",
 });
 
